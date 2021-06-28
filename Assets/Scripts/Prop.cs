6916b0c701ff733a2d8cc6518b233ba858797b5a
Boyi <2809984835@qq.com>
2021-06-28 09:22:48 +0800

    添加 道具脚本

---
+++ Assets/Scripts/Prop.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Prop : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    // Use this for initialization
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag(Tag.BombEffect))
+        {
+            //开启道具触发器
+            this.GetComponent<BoxCollider2D>().isTrigger = true;
+            //TODO
+        }
+    }
+}
