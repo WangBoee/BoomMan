761f3a0bf9252bdc5e66a9f14df46c2407739890
Boyi <2809984835@qq.com>
2021-06-30 15:32:00 +0800

    添加 UI

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIController : MonoBehaviour
+{
+    public Text txtHP;
+    public Text txtLevel;
+    public Text txtTime;
+    public Text txtEnemy;
+
+    //刷新UI
+    public void ReFresh(int hp, int level, int time, int enemy)
+    {
+        txtHP.text = "HP:" + hp.ToString();
+        txtLevel.text = "Level:" + level.ToString();
+        txtTime.text = "Time:" + time.ToString();
+        txtEnemy.text = "Enemy" + enemy.ToString();
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
